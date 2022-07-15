719361beeacfa5d7410014b3bff8f6bc2a712a9c
Luke Hinds <lhinds@redhat.com>
2022-07-15 10:06:50 +0100

    Test CI

--- foo.py
+++ foo.py
@@ -4,7 +4,7 @@ import sys
 def echo(phrase: str) -> None:
    """A dummy wrapper around print."""
    # for demonstration purposes, you can imagine that there is some
-   # valuable and reusable logic inside this function
+   # valuable and reusable logic inside this function.
    print(phrase)
 
 def main() -> int:
